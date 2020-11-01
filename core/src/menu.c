d38f02b6c536aca47d4ed6a854253dde1ade4ad4
Quentin Pierson <45620494+quentin-pierson@users.noreply.github.com>
2020-11-01 14:20:30 +0100

    Addition of insert file and record display functions.
    Insert file will ask for your full path to your dtd and xml file, you should give a name for each one that will be registered. This recording will allow you to find yourself in your recordings
    Recording display : used to display all the recordings made before

--- core/src/menu.c
+++ core/src/menu.c
@@ -29,5 +29,70 @@ void menu(){
     }
 
   }while (choice !=3);
-} 
+}
+
+void insertFile(){
+  
+  //Partie XML
+
+  //Partie PathXml
+  printf("Donnes moi le chemin complet de ton fichier XML: ");
+  fgets(filePathXml,20,stdin);
+  fflush(stdin);
+
+  /*
+  ATTENTION
+  Faire une detection du fichier inexistant
+  */
+
+  //Partie NomXML
+  printf("Donnes moi un nom pour ton fichier XML: ");
+  fgets(nameXml[increment],20,stdin);
+  fflush(stdin); 
+
+ 
+  // Partie DTD 
+
+  //Partie PathDTD
+  printf("Donne moi le chemin complet de ta DTD: ");
+  fgets(filePathDtd,20,stdin);
+  fflush(stdin);
+
+  /*
+  ATTENTION
+  Faire une detection du fichier inexistant
+  */
+
+  //Partie NomDTD
+  printf("Donnes moi un nom pour ta DTD: ");
+  fgets(nameDtd[increment],20,stdin);
+  fflush(stdin);
+
+  increment++; // on l'increment car on as un nouvelle enregistrement
+
+  //mettre un if si pas bien enregistrer 
+
+  // Affichage des fichiers et noms
+  //printf("%s%s",filePathXml,nameXml);
+  //printf("%s%s",filePathDtd,nameDtd);
+
+  printf("\n");
+
+}
+
+void afficheEnregistrement(){
+  printf("Voici les enregistrement: \n");
+  int valide = 1;
+  for(int i=0;i<increment;i++){ // Parcours les enregistrements si aucun enregistrement alors on vérifie le if 
+    if(strlen(nameXml[i])>0 && strlen(nameDtd[i])>0){ // si la taille du tableau>0 alors il existe un enregistrement
+      valide=0; //valide = 0 car on est rentré dans le if et pas besoin d'afficher aucun enregistrement
+      nameXml[i][strlen(nameXml[i]) -1] = '\0'; // Permets de prendre le dernier caractère et le remplacer par un \0  
+      printf("%s-%s",nameXml[i],nameDtd[i]); // Affichage des enregistrements sur la même ligne grace au remplacement du \0
+    }
+  }
+  if(valide==1){ // si aucun enregistrement on affiche : 
+    printf("Aucun enregistrement veuillez en faire un "); 
+  }
+  printf("\n");
+}
 
