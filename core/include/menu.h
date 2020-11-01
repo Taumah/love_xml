18283baa6dbfe0d69f05ccbf93e53f7d15af2a6c
Quentin Pierson <45620494+quentin-pierson@users.noreply.github.com>
2020-11-01 14:21:31 +0100

    Adding functions insertFile and afficheEnregistrement

--- core/include/menu.h
+++ core/include/menu.h
@@ -4,3 +4,14 @@
 
 /* affiche le menu */
 void menu();
+
+/*
+Cette fonction va demander à l'utilisateur d'entrer un nom, un chemin pour la DTD et le fichier XML.
+*/ 
+void insertFile();
+
+/* 
+Cette fonction sert à afficher les enregistrements.
+Si aucun n'enregistrement est précent alors on n'affiche rien 
+*/ 
+void afficheEnregistrement();
