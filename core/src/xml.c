2ed114a835e860cfc4d55db04a91ae93e42699bf
repl.it user <replituser@example.com>
2020-11-15 17:03:01 +0000

    Modification of Read Xml to allow returning a char *

--- core/src/xml.c
+++ core/src/xml.c
@@ -3,32 +3,58 @@
 
 extern fileAsArray f_xml;
 
-void readXML(char* fileName){
+char *readXML(char* fileName){
+  char *fileXml = NULL;
+  int str_size , read_size = 0;
+  
+  if(verifExtension(fileName) == 1){
+    return;
+  }
+  
+  FILE* XML = fopen(fileName , "rw");
 
-    if(verifExtension(fileName) == 1){
-        return;
-    }
+  if( XML == NULL){
+    printf("erreur à l'ouverture");
+    exit(1);
+  }
 
-    FILE* XML = fopen(fileName , "rw");
+  if (XML) {
+    // permet de se mettre a la fin du fichier
+    fseek(XML,0,SEEK_END);
+  
+    // calculer la taille du fichier 
+    str_size = ftell(XML);
 
-    if( XML == NULL){
-        printf("erreur à l'ouverture");
-        exit(1);
-    }
+    //retourner au debut du fichier
+    //rewind(XML);
+    fseek(XML,0,SEEK_SET);
+
+    // creation d'un char dynamique de longeur du fichier+1
+    fileXml = malloc(sizeof(char) * str_size+1);
+
+    // permet de lire tout lezz fichier en une seule fois
+    read_size = fread(fileXml, sizeof(char), str_size, XML);
+
+    //permet de transfromer fileXml en vrai chaine de caractre 
+    fileXml[str_size] = '\0';
+
     
-    int line_number = 0;
-      
-
-    while (  fgets( f_xml.array[line_number]  , 200 , XML) != NULL ){
-        line_number+= 1;    
+    if (str_size != read_size)
+    {
+      /*
+        S'il y a eu une erreur alors on supprime tout.
+      */
+      free(fileXml);
+      fileXml = NULL;
     }
 
-    f_xml.length = line_number;
+    // A la fin on ferme le fichier pour eviter un probleme par la suite
+    fclose(XML);
+  }
 
-    fclose(XML);    
-
+  // On retourne notre fileXml pour pouvoir l'utiliser par la suite 
+  return fileXml;
 }
-
 
 const char *getFilenameExt(const char *filename) {
     const char *dot = strrchr(filename, '.');
@@ -48,3 +74,32 @@ int verifExtension(char* fileXML){
     }
 }
 
+
+
+char *recoverValue(char* fileName){
+  /*
+  Cette fonction prend char par char et regarder si il tombe pas sur <
+  si il le trouve on ajoute la suite jusqu'a qu'il trouve > dans un tableau et on le print 
+  */
+
+  char *buffer = readXML(fileName);
+  int i=0;
+  char tab[256];
+
+  if(buffer == 1){
+    return;
+  }
+
+  while(buffer[i] !='\0'){
+      if(buffer[i]=='<'){
+        tab[i] = buffer[i];
+        i++;
+    }
+    else if(buffer[i] =='>'){
+      tab[i] = buffer[i];
+      i++;
+    }
+    i++;
+  }
+  return tab;
+}
